fe0d9fd44a9903c8579955738b4d44903c1a893a
rajrishabh01 <raj.rishabh01@gmail.com>
2018-07-25 16:04:02 +0530

    Using this reference

---
+++ this/src/com/deloitte/main/MainClass.java
@@ -0,0 +1,12 @@
+package com.deloitte.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ScopeExample scope = new ScopeExample();
+		scope.firstMethod();
+		
+
+	}
+
+}
